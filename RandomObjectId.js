984fbe9aaaa23acc467c38b7c8b638810c553402
Sergiy Tertychnij <sergiy@qubyx.com>
2017-03-21 15:49:08 +0200

    Random objectId function

---
+++ RandomObjectId.js
@@ -0,0 +1,8 @@
+//This function generate random ObjectId. Very useful for tests
+
+exports.randObjId = function () {
+    var timestamp = (new Date().getTime() / 1000 | 0).toString(16);
+    return timestamp + 'xxxxxxxxxxxxxxxx'.replace(/[x]/g, function() {
+        return (Math.random() * 16 | 0).toString(16);
+    }).toLowerCase();
+};
